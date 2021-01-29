300906e70eb9c122707521922f66bddee4ff1926
Vick Kumar <vickkumar@rivian.com>
2021-01-29 11:58:54 -0500

    fixed home issue

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,26 +6,26 @@ const Header = () => {
     return (
         <Suspense fallback={'Loading...'}>
             <BrowserRouter>
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <div class="container-fluid">
-                        <a class="navbar-brand" href="#">POC</a>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <div className="container-fluid">
+                        <a className="navbar-brand" href="#">POC</a>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
                         </button>
-                        <div class="collapse navbar-collapse" id="navbarNav">
-                            <ul class="navbar-nav">
-                                <li class="nav-item">
-                                    <a class="nav-link" aria-current="page" href="#">Home</a>
+                        <div className="collapse navbar-collapse" id="navbarNav">
+                            <ul className="navbar-nav">
+                                <li className="nav-item">
+                                    <a className="nav-link" aria-current="page" href="#">Home</a>
                                 </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">User</a>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">User</a>
                                 </li>
                             </ul>
                         </div>
                     </div>
                 </nav>
                 <Routes>
-                    <Route path="home/*" element={<Home />} />
+                    <Route path="/" element={<Home />} />
                 </Routes>
             </BrowserRouter>
         </Suspense>
