b37b33698c7eb71dcaadbcc65046da24c856755e
Vick Kumar <vickkumar@rivian.com>
2021-01-29 14:39:32 -0500

    fixed bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
 import Header from './components/Header'
 import "./index.css";
-
+import "../node_modules/bootstrap/dist/css/bootstrap.css"
 const App = () => {
     return (
         <Header />
--- src/index.js
+++ src/index.js
@@ -1,3 +1,1 @@
-import 'bootstrap/dist/css/bootstrap.css';
-
 import("./App");
