f46d1651dc26ea6df408ad92a925de7df0a38553
Vick Kumar <vickkumar@rivian.com>
2021-01-29 13:16:21 -0500

    made changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,16 @@
-import React, {Suspense} from 'react'
+import React, { Suspense } from 'react'
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 const Home = React.lazy(() => import("home/Home"));
 const User = React.lazy(() => import("user/User"));
+import Page from './Page'
 
 const Header = () => {
     return (
         <Suspense fallback={'Loading...'}>
             <BrowserRouter>
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                     <div className="container-fluid">
-                        <a className="navbar-brand" href="#">POC</a>
+                        <Link className="navbar-brand" to="/">POC</Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
@@ -19,13 +20,14 @@ const Header = () => {
                                     <Link className="nav-link" aria-current="page" to="home">Home</Link>
                                 </li>
                                 <li className="nav-item">
-                                <Link className="nav-link" aria-current="page" to="user">User</Link>
+                                    <Link className="nav-link" aria-current="page" to="user">User</Link>
                                 </li>
                             </ul>
                         </div>
                     </div>
                 </nav>
                 <Routes>
+                    <Route path="/" element={<Page />} />
                     <Route path="/home" element={<Home />} />
                     <Route path="/user" element={<User />} />
                 </Routes>
---
+++ src/components/Page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = () => {
+    return (
+        <h1>this page is in the shell app</h1>
+    )
+}
+
+export default Page
