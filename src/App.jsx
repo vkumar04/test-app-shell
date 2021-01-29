4b7cd3bbfcd6b9347e33bd96ac4cae63a7a868b6
Vick Kumar <vickkumar@rivian.com>
2021-01-29 11:05:18 -0500

    created header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 
-const App = () => <div>Hi there, I'm React from Webpack 5.</div>;
+const App = () => {
+    return (
+        <Suspense fallback={'Loading...'}>
+            <BrowserRouter>
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="#">POC</a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNav">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a class="nav-link" aria-current="page" href="#">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">User</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+                <Routes>
+                    <Route path="home/*" />
+                </Routes>
+            </BrowserRouter>
+        </Suspense>
+    )
+}
 
 ReactDOM.render(<App />, document.getElementById("app"));
